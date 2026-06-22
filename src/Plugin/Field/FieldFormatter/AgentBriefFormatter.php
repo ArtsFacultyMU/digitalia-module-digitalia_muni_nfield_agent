733b1d90d292d419eef42bd48c6fd36c9949a3f5
xadler1 <469190@mail.muni.cz>
2026-06-22 15:24:26 +0200

    Finish brief formatter

--- src/Plugin/Field/FieldFormatter/AgentBriefFormatter.php
+++ src/Plugin/Field/FieldFormatter/AgentBriefFormatter.php
@@ -19,24 +19,28 @@ use Drupal\digitalia_muni_nfield_agent\Plugin\Field\FieldType\AgentItem;
  *   field_types = {"dm_field_agent"},
  * )
  */
-final class AgentDefaultFormatter extends FormatterBase {
+final class AgentBriefFormatter extends FormatterBase {
 
   /**
    * {@inheritdoc}
    */
   public static function defaultSettings(): array {
-    return ['foo' => 'bar'] + parent::defaultSettings();
+    return [
+      'name_format' => 'simple_name',
+      ] + parent::defaultSettings();
   }
 
   /**
    * {@inheritdoc}
    */
   public function settingsForm(array $form, FormStateInterface $form_state): array {
-    $element['foo'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Foo'),
-      '#default_value' => $this->getSetting('foo'),
+    $element['name_format'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Name format'),
+      '#options' => $this->getNameFormatOptions(),
+      '#default_value' => $this->getSetting('name_format'),
     ];
+
     return $element;
   }
 
@@ -45,7 +49,7 @@ final class AgentDefaultFormatter extends FormatterBase {
    */
   public function settingsSummary(): array {
     return [
-      $this->t('Foo: @foo', ['@foo' => $this->getSetting('foo')]),
+      $this->t('Name format: @name_format', ['@name_format' => $this->getNameFormatOptions()[$this->getSetting('name_format')]]),
     ];
   }
 
@@ -56,163 +60,59 @@ final class AgentDefaultFormatter extends FormatterBase {
     $element = [];
 
     foreach ($items as $delta => $item) {
+      $orcid_ror = "";
+      $affiliation = "";
+      $name = "$item->name";
 
-//      if ($item->role) {
-//        $allowed_values = AgentItem::allowedRoleValues();
-//        $element[$delta]['role'] = [
-//          '#type' => 'item',
-//          '#title' => $this->t('Role'),
-//          '#markup' => $allowed_values[$item->role],
-//        ];
-//      }
-//
-//      if ($item->agent_type) {
-//        $allowed_values = AgentItem::allowedAgentTypeValues();
-//        $element[$delta]['agent_type'] = [
-//          '#type' => 'item',
-//          '#title' => $this->t('Agent type'),
-//          '#markup' => $allowed_values[$item->agent_type],
-//        ];
-//      }
-//
-//      if ($item->agent_tid) {
-//        $element[$delta]['agent_tid'] = [
-//          '#type' => 'item',
-//          '#title' => $this->t('Agent TID'),
-//          '#markup' => $item->agent_tid,
-//        ];
-//      }
+      if ($item->agent_type == "person") {
+        if ($item->orcid) {
+          $orcid_ror .= " <a href=\"https://orcid.org/{$item->orcid}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ORCID-iD_icon_vector.svg\" width=\"16px\"/></a>";
+        }
+
+        if ($item->institution_affiliation) {
+          $affiliation = "; {$item->institution_affiliation}";
+          if (!empty($item->ror)) {
+            $affiliation .= " <a href=\"https://ror.org/{$item->ror}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ror-icon-rgb.svg\" width=\"20px\"/></a>";
+          }
+
+          if (!empty($item->department)){
+            $affiliation .= ", {$item->department}";
+          }
+        }
+      }
+
+      if ($item->agent_type == "organisation") {
+        if ($item->ror) {
+          $orcid_ror .= " <a href=\"https://ror.org/{$item->ror}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ror-icon-rgb.svg\" width=\"20px\"/></a>";
+        }
+      }
+
+      if ($item->first_names && $item->last_names) {
+        if ($this->getSetting('name_format') == "first_last") {
+          $name = "{$item->first_names} {$item->last_names}";
+        }
+        if ($this->getSetting('name_format') == "last_first") {
+          $name = "{$item->last_names}, {$item->first_names}";
+        }
+      }
 
       if ($item->name) {
         $element[$delta]['name'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Name'),
-          '#markup' => "{$item->name} <a href=\"https://orcid.org/{$item->orcid}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ORCID-iD_icon_vector.svg\" width=\"16px\"/></a>; {$item->institution_affiliation} <a href=\"https://ror.org/{$item->ror}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ror-icon-rgb.svg\" width=\"24px\"/></a>, {$item->department}",
+          //'#type' => 'item',
+          //'#title' => $this->t('Name'),
+          '#markup' => "{$name}{$orcid_ror}{$affiliation}",
         ];
       }
-
- //     if ($item->orcid) {
- //       $element[$delta]['orcid'] = [
- //         '#type' => 'item',
- //         '#title' => $this->t('ORCID'),
- //         '#markup' => "<a href=\"https://orcid.org/{$item->orcid}\" target=\"_blank\">{$item->orcid}</a>",
- //       ];
- //     }
-
- //     if ($item->first_names) {
- //       $element[$delta]['first_names'] = [
- //         '#type' => 'item',
- //         '#title' => $this->t('First names'),
- //         '#markup' => $item->first_names,
- //       ];
- //     }
-
- //     if ($item->last_names) {
- //       $element[$delta]['last_names'] = [
- //         '#type' => 'item',
- //         '#title' => $this->t('Last names'),
- //         '#markup' => $item->last_names,
- //       ];
- //     }
-
- //     if ($item->ror) {
- //       $element[$delta]['ror'] = [
- //         '#type' => 'item',
- //         '#title' => $this->t('ROR'),
- //         '#markup' => "<a href=\"https://ror.org/{$item->ror}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ror-icon-rgb.svg\" width=\"32px\"/></a>",
- //         //'#markup' => "<a href=\"https://ror.org/{$item->ror}\" target=\"_blank\">{$item->ror}</a>",
- //       ];
- //     }
-
-      if ($item->institution_affiliation) {
-        $element[$delta]['institution_affiliation'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Affiliation'),
-          '#markup' => "{$item->institution_affiliation} <a href=\"https://ror.org/{$item->ror}\" target=\"_blank\"><img src=\"/themes/custom/islandora_muni/images/ror-icon-rgb.svg\" width=\"24px\"/></a>, {$item->department}",
-        ];
-      }
-
-//      if ($item->department_tid) {
-//        $element[$delta]['department_tid'] = [
-//          '#type' => 'item',
-//          '#title' => $this->t('Department TID'),
-//          '#markup' => $item->department_tid,
-//        ];
-//      }
-//
-//      if ($item->department) {
-//        $element[$delta]['department'] = [
-//          '#type' => 'item',
-//          '#title' => $this->t('Department'),
-//          '#markup' => $item->department,
-//        ];
-//      }
-
-      if ($item->contact) {
-        $element[$delta]['contact'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Contact'),
-          '#markup' => $item->contact,
-        ];
-      }
-
-      if ($item->alternative_id) {
-        $element[$delta]['alternative_id'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Alternative ID'),
-          '#markup' => $item->alternative_id,
-        ];
-      }
-
-      if ($item->alternative_id_type) {
-        $allowed_values = AgentItem::allowedAlternativeIDTypeValues();
-        $element[$delta]['alternative_id_type'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Alternative ID type'),
-          '#markup' => $allowed_values[$item->alternative_id_type],
-        ];
-      }
-
-      if ($item->link) {
-        $element[$delta]['link'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Link'),
-          'content' => [
-            '#type' => 'link',
-            '#title' => $item->link,
-            '#url' => Url::fromUri($item->link),
-          ],
-        ];
-      }
-
-      if ($item->note) {
-        $element[$delta]['note'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Note'),
-          '#markup' => $item->note,
-        ];
-      }
-
-      if ($item->private_note) {
-        $element[$delta]['private_note'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Private note'),
-          '#markup' => $item->private_note,
-        ];
-      }
-
-      if ($item->extra) {
-        $element[$delta]['extra'] = [
-          '#type' => 'item',
-          '#title' => $this->t('Extra'),
-          '#markup' => $item->extra,
-        ];
-      }
-
     }
 
     return $element;
   }
 
+  protected function getNameFormatOptions() {
+    return [
+        'simple_name' => "Name field",
+        'first_last' => "First-last",
+        'last_first' => "Last-first",
+    ];
+  }
 }
-
